ad4dffbc6b8c99a192b6c923b06b77fea45e8e86
Erin Krengel <ekrengel@pulumi.com>
2019-09-30 15:01:06 -0700

    Make the template Policy for an AWS bucket

--- policy-pack-typescript/index.ts
+++ policy-pack-typescript/index.ts
@@ -18,13 +18,14 @@ import * as assert from "assert";
 
 new PolicyPack("policy-pack-typescript", {
     policies: [{
-        name: "example-policy",
-        description: "An example advisory policy.",
-        enforcementLevel: "advisory",
+        name: "s3-no-public-read",
+        description: "Prohibits setting the publicRead or publicReadWrite permission on AWS S3 buckets.",
+        enforcementLevel: "mandatory",
         rules: [
-            typedRule(aws.ec2.Instance.isInstance, it => {
-                assert(!it.instanceType.includes("xlarge"));
-            }),
+            typedRule(aws.s3.Bucket.isInstance, it => assert.ok(it.acl !== "public-read"
+                && it.acl !== "public-read-write",
+                "You cannot set public-read or public-read-write on an S3 bucket. " +
+                "Read more about ACLs here: https://docs.aws.amazon.com/AmazonS3/latest/dev/acl-overview.html")),
         ],
     }],
 });
