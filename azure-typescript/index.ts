2af682235a23037999826a4d185d84228ecacb25
Justin Van Patten <jvp@justinvp.com>
2019-11-13 17:21:25 -0800

    Updates for @pulumi/policy v0.2.0 APIs

--- azure-typescript/index.ts
+++ azure-typescript/index.ts
@@ -1,18 +1,18 @@
 import * as azure from "@pulumi/azure";
-import { PolicyPack, typedRule } from "@pulumi/policy";
-import * as assert from "assert";
+import { PolicyPack, validateTypedResource } from "@pulumi/policy";
 
 new PolicyPack("azure-typescript", {
     policies: [{
         name: "storage-container-no-public-read",
         description: "Prohibits setting the public permission on Azure Storage Blob Containers.",
         enforcementLevel: "mandatory",
-        rules: [
-            typedRule(azure.storage.Container.isInstance, it => assert.ok(it.containerAccessType !== "blob"
-                && it.containerAccessType !== "container",
-                "You cannot set blob or container access on an Azure Storage Container. " +
-                "Read more about read access here: " +
-                "https://docs.microsoft.com/en-us/azure/storage/blobs/storage-manage-access-to-resources")),
-        ],
+        validateResource: validateTypedResource(azure.storage.Container, (container, args, reportViolation) => {
+            if (container.containerAccessType === "blob" || container.containerAccessType === "container") {
+                reportViolation(
+                    "You cannot set blob or container access on an Azure Storage Container. " +
+                    "Read more about read access here: " +
+                    "https://docs.microsoft.com/en-us/azure/storage/blobs/storage-manage-access-to-resources");
+            }
+        }),
     }],
 });
