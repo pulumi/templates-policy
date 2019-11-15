ef8c76d8864889cec84d9ae6fee4b2b6a6a336da
Erin Krengel <ekrengel@pulumi.com>
2019-11-15 11:55:19 -0800

    Add K8s typescript template

---
+++ kubernetes-typescript/index.ts
@@ -0,0 +1,17 @@
+import * as k8s from "@pulumi/kubernetes";
+import { PolicyPack, validateTypedResource } from "@pulumi/policy";
+
+new PolicyPack("kubernetes-typescript", {
+    policies: [{
+        name: "no-public-services",
+        description: "Kubernetes Services should be cluster-private.",
+        enforcementLevel: "mandatory",
+        validateResource: validateTypedResource(k8s.core.v1.Service, (svc, args, reportViolation) => {
+            if (svc.spec && svc.spec.type === "LoadBalancer") {
+                reportViolation("Kubernetes Services cannot be of type LoadBalancer, which are exposed to " +
+                    "anything that can reach the Kubernetes cluster. This likely including the " +
+                    "public Internet.");
+            }
+        }),
+    }],
+});
