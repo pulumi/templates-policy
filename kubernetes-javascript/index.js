47716746b024933cab713c9f5caf0072297f5bd5
Justin Van Patten <jvp@justinvp.com>
2020-02-10 13:55:33 -0800

    Replace uses of validateTypedResource with validateResourceOfType

--- kubernetes-javascript/index.js
+++ kubernetes-javascript/index.js
@@ -7,7 +7,7 @@ new policy.PolicyPack("kubernetes-typescript", {
         name: "no-public-services",
         description: "Kubernetes Services should be cluster-private.",
         enforcementLevel: "mandatory",
-        validateResource: policy.validateTypedResource(k8s.core.v1.Service, (svc, args, reportViolation) => {
+        validateResource: policy.validateResourceOfType(k8s.core.v1.Service, (svc, args, reportViolation) => {
             if (svc.spec && svc.spec.type === "LoadBalancer") {
                 reportViolation("Kubernetes Services cannot be of type LoadBalancer, which are exposed to " +
                     "anything that can reach the Kubernetes cluster. This likely including the " +
