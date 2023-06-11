fc919833cb0f4346b44ab325a025f42289be8c36
Aurelien Requiem <aurelien@requiem.fr>
2023-06-11 18:01:32 +1000

    📝 improve inline doc and template description

--- aws-premium-policies-typescript/index.ts
+++ aws-premium-policies-typescript/index.ts
@@ -2,9 +2,10 @@ import { PolicyPack } from "@pulumi/policy";
 import { policyManager } from "@pulumi-premium-policies/policy-manager";
 
 /**
- * To use Pulumi Premium Policies (beta), please look into the README.md for more information.
+ * 📝
+ * To use Pulumi Premium Policies (beta),
+ * please read the README.md file for more information.
  */
-
 new PolicyPack("aws-premium-policies-typescript", {
     policies:[
         ...policyManager.selectPolicies({
@@ -17,6 +18,10 @@ new PolicyPack("aws-premium-policies-typescript", {
     ],
 });
 
+/**
+ * Optional✔️: Display additional stats and helpful
+ * information when the policy pack is evaluated.
+ */
 policyManager.displaySelectionStats({
     displayGeneralStats: true,
     displayModuleInformation: true,
