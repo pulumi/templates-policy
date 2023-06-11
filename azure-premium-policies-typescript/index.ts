b751cbecf424be47608e753a33444be9ebe8d87d
Aurelien Requiem <aurelien@requiem.fr>
2023-06-11 18:26:13 +1000

    ✨ add template for Azure Premium Policies

---
+++ azure-premium-policies-typescript/index.ts
@@ -0,0 +1,29 @@
+import { PolicyPack } from "@pulumi/policy";
+import { policyManager } from "@pulumi-premium-policies/policy-manager";
+
+/**
+ * 📝
+ * To use Pulumi Premium Policies (beta),
+ * please read the README.md file for more information.
+ */
+new PolicyPack("aws-premium-policies-typescript", {
+    policies:[
+        ...policyManager.selectPolicies({
+            // vendors: ["azure"],
+            // services: ["compute", "containerservice", "storage"],
+            // severities: ["medium", "high", "critical"],
+            // topics: ["encryption"],
+            // frameworks: ["pcidss"]
+        }, "mandatory" ),
+    ],
+});
+
+/**
+ * Optional✔️: Display additional stats and helpful
+ * information when the policy pack is evaluated.
+ */
+policyManager.displaySelectionStats({
+    displayGeneralStats: true,
+    displayModuleInformation: true,
+    displaySelectedPolicyNames: true,
+});
